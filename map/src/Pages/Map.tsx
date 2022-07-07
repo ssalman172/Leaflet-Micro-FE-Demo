f0dd83405f292301a21fee8264a62b8df4d2b721
Salman S <salmanshalahuddin133@gmail.com>
2022-07-07 11:23:38 +0700

    :lipstick: added img overlay

--- map/src/Pages/Map.tsx
+++ map/src/Pages/Map.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, Marker, TileLayer, ImageOverlay } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import { renderToStaticMarkup } from 'react-dom/server';
-import { divIcon } from 'leaflet';
+import L, { divIcon } from 'leaflet';
 
 const Wrapper = styled.main`
   display: flex;
@@ -31,20 +31,19 @@ const Map = () => {
   const customMarkerIcon = divIcon({
     html: iconMarkup,
   });
+  const latLngBounds = L.latLngBounds([[-6.948658102374332, 107.61851850179244], [-6.9506534302708305, 107.62115493245045]]);
 
   return (
     <Wrapper>
       <MapWrapper>
         <Title>The Map</Title>
-        <MapContainer center={[-6.9496521, 107.6174784]} zoom={17} scrollWheelZoom={true}>
+        <MapContainer center={[-6.949496719488826, 107.61966920913646]} zoom={17} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[-6.9496521, 107.6174784]} icon={customMarkerIcon}>
-            <Popup>
-              PT Len Industri
-            </Popup>
+          <Marker position={[-6.949496719488826, 107.61966920913646]} icon={customMarkerIcon}>
+            <ImageOverlay url="./assets/len.jpg" bounds={latLngBounds} />
           </Marker>
         </MapContainer>
       </MapWrapper>
